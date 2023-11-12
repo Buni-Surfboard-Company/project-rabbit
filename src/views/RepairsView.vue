a6ccd04fed5a4ee385be8deb55fe73f7439edd41
parkercon <61677313+parkercon@users.noreply.github.com>
2023-11-12 15:33:22 -0800

    added repair info

--- src/views/RepairsView.vue
+++ src/views/RepairsView.vue
@@ -1,19 +1,38 @@
 <template>
-<div class="pt-5" style="margin: auto; width: 75%;">
-  <contact-form/>
+<router-link class="no-underline" to="/contact">
+  <Button label="Contact Us" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>
+</router-link>
+<div class="repair-pdf" style="margin: auto; width: 75%; min-height: 75vh; margin-top: 25px;">
+  <div class="pt-5" style="margin: auto; width: 75%; min-height: 100%;">
+
+</div>
 </div>
 </template>
 
 <script>
-import ContactForm from '@/components/ContactForm.vue'
+
 export default {
-    components: {
-        ContactForm
+  components: {
+  },
+    // components: {
+    //     ContactForm
+    // }
+    data(){
+      return{
+  }
     }
 
 }
 </script>
 
 <style>
-
+.repair-pdf{
+    background-image: url("@/assets/images/repair-page.png");
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+.content-wrap{
+  min-height: 100vh;
+}
 </style>
