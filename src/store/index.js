b5a926ad69ecb2088ebe0a2dbf52c43d92a88f37
parkercon <61677313+parkercon@users.noreply.github.com>
2023-12-27 20:45:31 -0800

    added blog infrastructure

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import { createStore } from 'vuex'
 
+
 export default createStore({
   state: {
-  },
-  getters: {
+    blogPosts: [], // Initialize with an empty array
   },
   mutations: {
+    setBlogPosts(state, posts) {
+      state.blogPosts = posts;
+    },
   },
   actions: {
+    async fetchBlogPosts({ commit }) {
+      // Fetch blog posts from the static file (replace with your file path)
+      const response = await fetch('/blogposts.json');
+      const posts = await response.json();
+      commit('setBlogPosts', posts);
+    },
   },
-  modules: {
-  }
+  getters: {
+    getBlogPostById: (state) => (id) => {
+      return state.blogPosts.find((post) => post.id == id);
+    },
+  },
 })
